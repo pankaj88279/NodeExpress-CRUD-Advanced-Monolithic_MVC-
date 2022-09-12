0f3afea02e075ba549d2090a051b635cfb157728
pankaj88279 <pankajjawi1993@gmail.com>
2022-09-12 11:44:58 +0530

    new

--- src/controllers/loginControllers.js
+++ src/controllers/loginControllers.js
@@ -6,10 +6,14 @@ const{User}=require('../models/user')
 let loginController=(req,res)=>{
     
     console.log(req.body)
-      
-     const user = new User(req.body)
-    user.save()
+    
+    User.findOne({email:req.body.email})
     .then((d)=>{
+        console.log(d)
+        if(d ===null){
+            const user = new User(req.body)
+        user.save()
+       .then((d)=>{
         res.status(200).json({
             msg:"login successfully",
             data:d
@@ -17,13 +21,38 @@ let loginController=(req,res)=>{
 
     })
     .catch((e)=>{
-        res.status(400).json({
-            msg:"data not saved",
+        res.status(403).json({
+            msg:"login unsccessful",
             error:e
         })
 
     })
+
+        }else{
+            res.status(404).json({
+                msg:"login unsccessful",
+                
+            })
+
         }
+    })
+    .catch((e)=>{
+        res.status(404).json({
+            msg:"error"
+            
+        })
+        
+    })
+    
+        
+        
+
+    }
+
+    
+
+    
+        
         
     
     
--- src/controllers/registerControllers.js
+++ src/controllers/registerControllers.js
@@ -1,27 +1,56 @@
 
 const express=require('express');
 const { User } = require("../models/user")
+const bcrypt = require('bcrypt');
 
 
 
 let registerController=(req,res)=>{
 
-    const user = new User(req.body)
-        user.save()
+    User.findOne({email:req.body.email})
     .then((d)=>{
-        res.status(200).json({
-            msg:"register successfully",
-            data:d
-        })
+        console.log(d);
+        if(d ===null){
+         const salt = 10;
+           
+       const hash = bcrypt.hashSync(req.body.password_hash, salt);
+       req.body.password_hash=hash;
 
+        const user = new User(req.body)
+        user.save()
+        .then((d)=>{
+            res.status(200).json({
+                msg:"register successfully",
+                data:d
+            })
+        })
+        .catch((e)=>{
+            res.status(400).json({
+                msg:"not register successfully",
+                error:e
+            })
+        })
+    
+        }else{
+            res.status(404).json({
+                msg:"email already exits",
+                
+            })
+
+        }
     })
     .catch((e)=>{
-        res.status(400).json({
-            msg:"not register successfully",
-            error:e
+        res.status(403).json({
+            msg:"error"
         })
+       
     })
+   
+      
     }
+    
+
+    
     
     
     
