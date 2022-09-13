0243d80a6affb38d9d0000a0293694f65d1c7b41
pankaj88279 <pankajjawi1993@gmail.com>
2022-09-13 11:09:38 +0530

    Added a Procfile.

--- src/controllers/loginControllers.js
+++ src/controllers/loginControllers.js
@@ -1,23 +1,29 @@
 
 const express=require('express')
-
+const jwt =require('jsonwebtoken');
 const{User}=require('../models/user')
 
 const bcrypt = require('bcrypt');
+
 let loginController=(req,res)=>{
     
-    console.log(req.body)
+    // console.log(req.body)
+    var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
     
     User.findOne({email:req.body.email})
     .then((d)=>{
-        console.log(d)
+        // console.log(d)
         if(d ===null){
             const user = new User(req.body)
         user.save()
        .then((d)=>{
+        // var token = jwt.sign(req.body, process.env.JWT_SECRET);
+        // var token = jwt.sign(req.body, process.env.JWT_SECRET);
+     
         res.status(403).json({
             msg:"invalid",
-            data:req.body
+            data:req.body,
+            token
         })
 
     })
@@ -30,13 +36,14 @@ let loginController=(req,res)=>{
     })
 
         }else{
-            console.log("d",d.password_hash)
-            if(bcrypt.compareSync(req.body.password_hash, d.password_hash,)){
+            // console.log("d",d)
+            const x=bcrypt.compareSync(req.body.password_hash, d.password_hash,)
+            if(bcrypt.compareSync(req.body.password_hash, d.password_hash)){
 
                 res.status(200).json({
                     result:"welcome",
                     msg:"invalid",
-                    data:req.body
+                    data:d
                 })
             }else{
                 res.status(404).json({
--- src/controllers/registerControllers.js
+++ src/controllers/registerControllers.js
@@ -1,6 +1,7 @@
 
 const express=require('express');
 const { User } = require("../models/user")
+
 const bcrypt = require('bcrypt');
 
 
